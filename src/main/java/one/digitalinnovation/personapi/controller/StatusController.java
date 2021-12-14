44cbdd1e138c285f0a1eb68f7cb199f131d09120
Henrique <ap_henrique@hotmail.com>
2021-12-13 21:53:25 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/personapi/controller/StatusController.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/status")
+public class StatusController {
+
+    @GetMapping
+    public String getStatus() {
+        return "Server is ON!";
+    }
+    
+}
